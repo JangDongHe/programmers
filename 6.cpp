91491c90e0b46863db274f65580b86f360c649a5
동희 <dkden0911@gmail.com>
2024-01-31 17:03:07 +0900

    2024.01.31

---
+++ 6.cpp
@@ -0,0 +1,10 @@
+//특수문자 출력 !@#$%^&*(\'"<>?:;
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    //cout << "!@#$%^&*(\\'\"<>?:;";
+    cout << R"(!@#$%^&*(\'"<>?:;)";
+    return 0;
+}
---
+++ 7.cpp
@@ -0,0 +1,13 @@
+//문자열 바꿔치기
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    answer = my_string.replace(s,overwrite_string.size(),overwrite_string);
+    return answer;
+}
